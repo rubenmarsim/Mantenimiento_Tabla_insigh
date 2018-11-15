0cdce4f6102bd836cdaf44f2fc2b0019a90dba27
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2018-11-15 18:26:11 +0100

    insertar fila

--- MantenimientoTest/Form1.cs
+++ MantenimientoTest/Form1.cs
@@ -26,7 +26,6 @@ namespace MantenimientoTest
         public frmMant_Table()
         {
             InitializeComponent();
-            Inicializaciones();
         }
         #endregion
 
@@ -35,7 +34,7 @@ namespace MantenimientoTest
         {
             CDB = new ClassDB();
             CSDStxtBox = new SdsTextBox();
-            dts = CDB.portaPerConsulta("select * from UserTypes");
+            dts = CDB.portaPerConsulta(query);
             RellenarDataGrid(dts);
             dgvMant_table.AllowUserToAddRows = false;
         }
@@ -63,38 +62,34 @@ namespace MantenimientoTest
                 if (sdsControl is SdsTextBox)
                 {
                     ((SdsTextBox)sdsControl).DataBindings.Clear();
-                    sdsControl.Text = "";
-                        
+                    sdsControl.Text = "";             
                 }
             }
         }
         private void AñadirFila()
         {
+            string nomCamp = ""; 
             DataRow dr = dts.Tables[0].NewRow();
             foreach (Control sdsControl in this.Controls)
             {
                 if (sdsControl is SdsTextBox)
                 {
-                    int i = 0;
-                    dr[i] = sdsControl.Text;
-                    i++;
+                    nomCamp = ((SdsTextBox)sdsControl).ColumnName;
+                    dr[nomCamp] = sdsControl.Text;
+                    
                 }
             }
-            dts.Tables[0].Rows.Add(dr);//NewRow(dr);//Add(dr);
-            dts.AcceptChanges();
-
-            //DataTable dt = dts.Tables[0];
-            //DataRow dr = dt.NewRow();
-            //dt.Rows.Add();
-            //dt.AcceptChanges();
-
+            dts.Tables[0].Rows.Add(dr);
         }
         #endregion
 
         #region Eventos
         private void validar(object sender, EventArgs e)
         {
-            ((TextBox)sender).DataBindings[0].BindingManagerBase.EndCurrentEdit();
+            if (!EsNou)
+            {
+                ((TextBox)sender).DataBindings[0].BindingManagerBase.EndCurrentEdit();
+            }        
         }
 
         private void btnUpdate_Click(object sender, EventArgs e)
@@ -106,8 +101,8 @@ namespace MantenimientoTest
             else
             {
                 AñadirFila();
+                CDB.Actualitzar(dts, query);
             }
-
         }
 
         private void btnInsert_Click(object sender, EventArgs e)
@@ -115,10 +110,11 @@ namespace MantenimientoTest
             QuitarBindDades();
             EsNou = true;
             txtBoxCodeType.Focus();
-            //AñadirFila();
-            //BindDades();
-            //var dg_r = dgvMant_table.Rows.Count;
 
+        }
+        private void frmMant_Table_Load(object sender, EventArgs e)
+        {
+            Inicializaciones();
         }
         #endregion
     }
